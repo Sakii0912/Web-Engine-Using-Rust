f37a34ea79d0fc7f87f8ae0ac5bb1b7deb3e6e24
Sakii0912 <ayyagari.saketh.09@gmail.com>
2025-07-02 13:05:01 +0530

    added comments to explain the code

--- web-engine/src/dom.rs
+++ web-engine/src/dom.rs
@@ -1,4 +1,4 @@
-struct Node {
+struct Node { // node of a DOM tree
     // data common to all nodes:
     children: Vec<Node>,
 
@@ -6,23 +6,23 @@ struct Node {
     node_type: NodeType,
 }
 
-enum NodeType {
+enum NodeType { // the type of node, which determines how it should be rendered
     Text(String),
     Element(ElementData),
 }
 
-struct ElementData {
+struct ElementData { // tag name and the attributes of that specific tag
     tag_name: String,
     attrs: AttrMap,
 }
 
-type AttrMap = HashMap<String, String>;
+type AttrMap = HashMap<String, String>; // attribute to value mapping
 
-fn text(data: String) -> Node {
+fn text(data: String) -> Node { // text nodes
     Node { children: Vec::new(), node_type: NodeType::Text(data) }
 }
 
-pub fn elem(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node {
+pub fn elem(tag_name: String, attrs: AttrMap, children: Vec<Node>) -> Node { // element nodes
     Node {
         children,
         node_type: NodeType::Element(ElementData { tag_name, attrs })
